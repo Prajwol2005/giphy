05cf0b9a43ea890088c24beb2d6eea271004fc42
Glitch (handy-cooperative-cream) <none>
2022-04-12 22:19:07 +0000

    ↔️🫓 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-let gify =
-  "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1";
-
 $(".search-button").click(function () {
+  let userInput = $("input").val();
+  console.log(userInput);
+
+  let gify =
+    `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1`;
   fetch(gify)
+  
     .then(function (response) {
       return response.json();
     })
 
     .then(function (data) {
-      console.log(data["data"][0]["url"]);
-      return 
+      console.log(data);
+    //var url = (data["data"][0]["embed_url"]);
+    //var url = "https://media4.giphy.com/media/njtPBlbYnHAHK/giphy-downsiz"
+    
+    var rando = Math.floor(Math.random() * 50);
+    var url = (data["data"][rando]["embed_url"]);
+    $('body').append(`<img src=${url}  crossorigin="anonymous">`);
+      return;
     });
 });
